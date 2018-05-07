62e30a8dbb57480385426ac1a8831469ae8f055a
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-05-07 16:52:27 -0700

    minor refacturing

--- user_data/strategies/ReinforcedQuickie.py
+++ user_data/strategies/ReinforcedQuickie.py
@@ -18,11 +18,12 @@ import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 import numpy  # noqa
 
-
 class ReinforcedQuickie(IStrategy):
     """
 
     author@: Gert Wohlgemuth
+
+    works on new objectify branch!
 
     idea:
         only buy on an upward tending market
@@ -39,7 +40,7 @@ class ReinforcedQuickie(IStrategy):
     stoploss = -0.05
 
     # Optimal ticker interval for the strategy
-    ticker_interval = 5
+    ticker_interval = '5m'
 
     # resample factor to establish our general trend. Basically don't buy if a trend is not given
     resample_factor = 12
@@ -49,28 +50,7 @@ class ReinforcedQuickie(IStrategy):
     EMA_LONG_TERM = 21
 
     def populate_indicators(self, dataframe: DataFrame) -> DataFrame:
-        # defines the reinforcement logic
-        # resampled dataframe to establish if we are in an uptrend, downtrend or sideways trend
-        df = dataframe.copy()
-        df = df.set_index(DatetimeIndex(df['date']))
-
-        ohlc_dict = {
-            'open': 'first',
-            'high': 'max',
-            'low': 'min',
-            'close': 'last'
-        }
-
-        df = df.resample(str(self.ticker_interval * self.resample_factor) + 'min', how=ohlc_dict).dropna(how='any')
-        df['resample_sma'] = ta.SMA(df, timeperiod=25, price='close')
-        df = df.drop(columns=['open', 'high', 'low', 'close'])
-        df = df.resample(str(self.ticker_interval) + 'min')
-        df = df.interpolate(method='time')
-
-        df['date'] = df.index
-
-        df.index = range(len(df))
-        dataframe = merge(dataframe, df, on='date', how='left')
+        dataframe = ReinforcedQuickie.resample(dataframe, self.ticker_interval, self.resample_factor)
 
         ##################################################################################
         # buy and sell indicators
@@ -123,37 +103,37 @@ class ReinforcedQuickie(IStrategy):
         """
         dataframe.loc[
             (
-                (
                     (
-                            (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
-                            (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
-                            (dataframe['close'] == dataframe['min']) &
-                            (dataframe['close'] <= dataframe['bb_lowerband'])
-                    )
-                    |
-                    # simple v bottom shape (lopsided to the left to increase reactivity)
-                    # which has to be below a very slow average
-                    # this pattern only catches a few, but normally very good buy points
-                    (
-                            (dataframe['average'].shift(5) > dataframe['average'].shift(4))
-                            & (dataframe['average'].shift(4) > dataframe['average'].shift(3))
-                            & (dataframe['average'].shift(3) > dataframe['average'].shift(2))
-                            & (dataframe['average'].shift(2) > dataframe['average'].shift(1))
-                            & (dataframe['average'].shift(1) < dataframe['average'].shift(0))
-                            & (dataframe['low'].shift(1) < dataframe['bb_middleband'])
-                            & (dataframe['cci'].shift(1) < -100)
-                            & (dataframe['rsi'].shift(1) < 30)
-                            & (dataframe['mfi'].shift(1) < 30)
+                            (
+                                    (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
+                                    (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
+                                    (dataframe['close'] == dataframe['min']) &
+                                    (dataframe['close'] <= dataframe['bb_lowerband'])
+                            )
+                            |
+                            # simple v bottom shape (lopsided to the left to increase reactivity)
+                            # which has to be below a very slow average
+                            # this pattern only catches a few, but normally very good buy points
+                            (
+                                    (dataframe['average'].shift(5) > dataframe['average'].shift(4))
+                                    & (dataframe['average'].shift(4) > dataframe['average'].shift(3))
+                                    & (dataframe['average'].shift(3) > dataframe['average'].shift(2))
+                                    & (dataframe['average'].shift(2) > dataframe['average'].shift(1))
+                                    & (dataframe['average'].shift(1) < dataframe['average'].shift(0))
+                                    & (dataframe['low'].shift(1) < dataframe['bb_middleband'])
+                                    & (dataframe['cci'].shift(1) < -100)
+                                    & (dataframe['rsi'].shift(1) < 30)
+                                    & (dataframe['mfi'].shift(1) < 30)
 
+                            )
                     )
-                )
-                # safeguard against down trending markets and a pump and dump
-                &
-                (
-                    (dataframe['volume'] < (dataframe['volume'].rolling(window=30).mean().shift(1) * 20)) &
-                    (dataframe['resample_sma'] < dataframe['close']) &
-                    (dataframe['resample_sma'].shift(1) < dataframe['resample_sma'])
-                )
+                    # safeguard against down trending markets and a pump and dump
+                    &
+                    (
+                            (dataframe['volume'] < (dataframe['volume'].rolling(window=30).mean().shift(1) * 20)) &
+                            (dataframe['resample_sma'] < dataframe['close']) &
+                            (dataframe['resample_sma'].shift(1) < dataframe['resample_sma'])
+                    )
             )
             ,
             'buy'] = 1
@@ -191,4 +171,27 @@ class ReinforcedQuickie(IStrategy):
             ,
             'sell'
         ] = 1
+        return dataframe
+
+    @staticmethod
+    def resample( dataframe, interval, factor):
+        # defines the reinforcement logic
+        # resampled dataframe to establish if we are in an uptrend, downtrend or sideways trend
+        df = dataframe.copy()
+        df = df.set_index(DatetimeIndex(df['date']))
+        ohlc_dict = {
+            'open': 'first',
+            'high': 'max',
+            'low': 'min',
+            'close': 'last'
+        }
+        df = df.resample(str(int(interval[:-1]) * factor) + 'min', how=ohlc_dict).dropna(
+            how='any')
+        df['resample_sma'] = ta.SMA(df, timeperiod=25, price='close')
+        df = df.drop(columns=['open', 'high', 'low', 'close'])
+        df = df.resample(interval[:-1] + 'min')
+        df = df.interpolate(method='time')
+        df['date'] = df.index
+        df.index = range(len(df))
+        dataframe = merge(dataframe, df, on='date', how='left')
         return dataframe
