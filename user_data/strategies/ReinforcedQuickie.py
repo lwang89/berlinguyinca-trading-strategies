c9a187c0de9c06a7273334ab63132e07f341e591
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-05-07 09:25:48 -0700

    optimized a bit. Test on your own risk!

--- user_data/strategies/ReinforcedQuickie.py
+++ user_data/strategies/ReinforcedQuickie.py
@@ -31,12 +31,12 @@ class ReinforcedQuickie(IStrategy):
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
-        "0": 0.1
+        "0": 0.01
     }
 
     # Optimal stoploss designed for the strategy
     # This attribute will be overridden if the config file contains "stoploss"
-    stoploss = -0.02
+    stoploss = -0.05
 
     # Optimal ticker interval for the strategy
     ticker_interval = 5
@@ -95,8 +95,11 @@ class ReinforcedQuickie(IStrategy):
         dataframe['min'] = ta.MIN(dataframe, timeperiod=self.EMA_MEDIUM_TERM)
         dataframe['max'] = ta.MAX(dataframe, timeperiod=self.EMA_MEDIUM_TERM)
 
+        dataframe['cci'] = ta.CCI(dataframe)
         dataframe['mfi'] = ta.MFI(dataframe)
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=7)
+
+        dataframe['average'] = (dataframe['close'] + dataframe['open'] + dataframe['high'] + dataframe['low']) / 4
 
         ##################################################################################
         # required for graphing
@@ -120,17 +123,39 @@ class ReinforcedQuickie(IStrategy):
         """
         dataframe.loc[
             (
-                (dataframe['volume'] < (dataframe['volume'].rolling(window=30).mean().shift(1) * 20)) &
-                (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
-                (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
-                (dataframe['close'] == dataframe['min']) &
-                (dataframe['close'] <= dataframe['bb_lowerband']) &
-                (dataframe['resample_sma'] < dataframe['close']) &
-                (dataframe['resample_sma'].shift(1) < dataframe['resample_sma']) &
-                (dataframe['mfi'] < 20) &
-                (dataframe['mfi'] < dataframe['rsi'])
+                (
+                    (
+                            (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
+                            (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
+                            (dataframe['close'] == dataframe['min']) &
+                            (dataframe['close'] <= dataframe['bb_lowerband'])
+                    )
+                    |
+                    # simple v bottom shape (lopsided to the left to increase reactivity)
+                    # which has to be below a very slow average
+                    # this pattern only catches a few, but normally very good buy points
+                    (
+                            (dataframe['average'].shift(5) > dataframe['average'].shift(4))
+                            & (dataframe['average'].shift(4) > dataframe['average'].shift(3))
+                            & (dataframe['average'].shift(3) > dataframe['average'].shift(2))
+                            & (dataframe['average'].shift(2) > dataframe['average'].shift(1))
+                            & (dataframe['average'].shift(1) < dataframe['average'].shift(0))
+                            & (dataframe['low'].shift(1) < dataframe['bb_middleband'])
+                            & (dataframe['cci'].shift(1) < -100)
+                            & (dataframe['rsi'].shift(1) < 30)
+                            & (dataframe['mfi'].shift(1) < 30)
 
-            ),
+                    )
+                )
+                # safeguard against down trending markets and a pump and dump
+                &
+                (
+                    (dataframe['volume'] < (dataframe['volume'].rolling(window=30).mean().shift(1) * 20)) &
+                    (dataframe['resample_sma'] < dataframe['close']) &
+                    (dataframe['resample_sma'].shift(1) < dataframe['resample_sma'])
+                )
+            )
+            ,
             'buy'] = 1
 
         return dataframe
@@ -143,15 +168,25 @@ class ReinforcedQuickie(IStrategy):
         """
         dataframe.loc[
             (
-                (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
-                (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
-                (dataframe['close'] >= dataframe['max']) &
-                (dataframe['close'] >= dataframe['bb_upperband']) &
-                (dataframe['mfi'] > 80)
+                    (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
+                    (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
+                    (dataframe['close'] >= dataframe['max']) &
+                    (dataframe['close'] >= dataframe['bb_upperband']) &
+                    (dataframe['mfi'] > 80)
             ) |
+
+            # always sell on eight green candles
+            # with a high rsi
             (
-                (dataframe['mfi'] > 80) &
-                (dataframe['rsi'] > 80)
+                    (dataframe['open'] < dataframe['close']) &
+                    (dataframe['open'].shift(1) < dataframe['close'].shift(1)) &
+                    (dataframe['open'].shift(2) < dataframe['close'].shift(2)) &
+                    (dataframe['open'].shift(3) < dataframe['close'].shift(3)) &
+                    (dataframe['open'].shift(4) < dataframe['close'].shift(4)) &
+                    (dataframe['open'].shift(5) < dataframe['close'].shift(5)) &
+                    (dataframe['open'].shift(6) < dataframe['close'].shift(6)) &
+                    (dataframe['open'].shift(7) < dataframe['close'].shift(7)) &
+                    (dataframe['rsi'] > 70)
             )
             ,
             'sell'
